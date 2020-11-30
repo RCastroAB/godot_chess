44a9aaa1a51e6ca233fad8dbc2d888c5975bec32
RCastroAB <rafsafira@gmail.com>
2020-11-30 09:41:32 -0300

    fully functional kludge

--- Piece.gd
+++ Piece.gd
@@ -1,12 +1,18 @@
 extends Node2D
+export var attacks = []
 export var moves = []
+export var special_moves = []
 export var repeat = false
+var type
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 export var grid_position = Vector2()
+var player;
 
+const SPRITES = 'res://sprites/'
 
+var moved = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -41,20 +47,89 @@ func knight_moves():
 		directions += [vec, revec, rerevec]
 	return directions
 
+func pawn_moves():
+	if player == 'black':
+		return [Vector2(0, 1)]
+	else:
+		return [Vector2(0, -1)]
 
-func create_piece(piecename):
+
+func pawn_special_moves():
+	if player == 'black':
+		return [Vector2(0, 2)]
+	else:
+		return [Vector2(0, -2)]
+
+func pawn_attacks():
+	if player == 'black':
+		return [Vector2(1,1), Vector2(-1,1)]
+	else:
+		return [Vector2(1, -1), Vector2(-1, -1)]
+
+func bishop_moves():
+	
+	var dir = Vector2(1,1)
+	var rot = Transform2D(
+		Vector2(0,1), Vector2(-1,0), Vector2(0,0))
+	var directions = [dir]	
+	for i in range(3):
+		dir = rot.basis_xform(dir)
+		directions += [dir]
+	return directions
+
+func rook_moves():
+	
+	var dir = Vector2(0,1)
+	var rot = Transform2D(
+		Vector2(0,1), Vector2(-1,0), Vector2(0,0))
+	var directions = [dir]	
+	for i in range(3):
+		dir = rot.basis_xform(dir)
+		directions += [dir]
+	return directions
+
+
+
+func create_piece(piecename, color):
 	var movesrepeat
+	player = color
+	var path = ''
+	if color == 'white':
+		path = 'white_'
+	type = piecename
 	match piecename:
 		'king':
 			moves = king_moves()
+			attacks = king_moves()
 			repeat = false
+			$Sprite.texture = load(SPRITES +path+'WIP_king.png')
 		'queen':
 			moves = king_moves()
+			attacks = king_moves()
 			repeat = true
+			$Sprite.texture = load(SPRITES +path+'WIP_queen.png')
 		'knight':
 			moves = knight_moves()
+			attacks = knight_moves()
 			repeat = false
-
+			$Sprite.texture = load(SPRITES +path+'fuglier_horse.png')
+		'pawn':
+			moves = pawn_moves()
+			attacks = pawn_attacks()
+			special_moves = pawn_special_moves()
+			repeat = false
+			$Sprite.texture = load(SPRITES +path+'WIP_pawn.png')
+		'bishop':
+			moves = bishop_moves()
+			attacks = bishop_moves()
+			repeat = true
+			$Sprite.texture = load(SPRITES +path+'WIP_pawn.png')
+		'rook':
+			moves = rook_moves()
+			attacks = rook_moves()
+			repeat = true
+			$Sprite.texture = load(SPRITES + path + 'WIP_rook.png')
+		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
@@ -66,7 +141,15 @@ func get_moves(randseed=0):
 #	for dir in directions:
 #		moves += [grid_position + dir]
 	return moves
+	
+func get_special_moves():
+	if not moved:
+		return special_moves
+	else:
+		return []
 
+func get_attacks():
+	return attacks
 
 func move_repeat():
 	return repeat
--- TileMap.gd
+++ TileMap.gd
@@ -6,42 +6,65 @@ var target = Vector2()
 export var speed = 0.2
 var tolerance = cell_size.x/4
 var valid_moves
+var valid_attacks
 var move_repeat
 var shape = Vector2(8,8)
+var game_end = false
+var piece_positions = {}
 
 var Piece = load('res://Piece.tscn')
+
+var current_player = 'white'
 
 func place_piece(piece, x,y):
 	var position = map_to_world(Vector2(x,y))
 	piece.position = position + Vector2(1,1) * cell_size/2	
 	piece.grid_position = Vector2(x,y)
+	print(piece.grid_position)
+	piece_positions[[x,y]] = piece
 
-func _ready():
-	print(Piece)
-	
+func create_piece(piecename, x, y, color):
 	var piece = Piece.instance()
-	place_piece(piece, 3, 0)
-	piece.create_piece('king')
+	place_piece(piece, x, y)
+	piece.create_piece(piecename, color)
 	add_child(piece)
-	print(piece.position)
-	piece = Piece.instance()
-	place_piece(piece, 4, 0)
-	piece.create_piece('queen')
-	add_child(piece)
-	print(piece.position)
+
+
+func fill_board(color):
+	var yoffset
+	var piecename
 	
-	piece = Piece.instance()
-	place_piece(piece, 6, 0)
-	piece.create_piece('knight')
-	add_child(piece)
-	print(piece.position)
-	piece = Piece.instance()
-	place_piece(piece, 1, 0)
-	piece.create_piece('knight')
-	add_child(piece)
-	print(piece.position)
+	if color == 'black':
+		yoffset = 0
+	else:
+		yoffset = 7
+	var piecenames = [
+		'rook', 
+		'knight',
+		'bishop',
+		'king',
+		'queen',
+		'bishop',
+		'knight',
+		'rook'
+	]
+	
+	for i in range(piecenames.size()):
+		piecename = piecenames[i]
+		create_piece(piecename, i, yoffset, color)
+	
+	
+	for i in range(8):
+		create_piece('pawn', i, abs(1- yoffset), color)
+	
 	
 	print(get_children())
+
+
+
+func _ready():
+	fill_board('black')
+	fill_board('white')
 	walking = false
 
 
@@ -55,6 +78,7 @@ func _process(delta):
 		if target == selected.position:
 			selected.grid_position = world_to_map(target)
 			walking = false
+			selected.moved = true
 			selected = null
 		elif target.length() > 0:
 			velocity = (target - selected.position).normalized() * speed
@@ -64,6 +88,33 @@ func _process(delta):
 				selected.position = target
 				velocity = Vector2()
 			
+
+func process_attacks(initial_position, attacks, repeat):
+	var end_positions = []
+	var intermediate_position = Vector2()
+	var num_steps = shape.x if repeat else 1
+	
+	for attack in attacks:
+		var colision = false
+		intermediate_position = initial_position
+		for steps in range(1, num_steps+1):
+			intermediate_position = intermediate_position + attack
+			if intermediate_position.x >= shape.x or intermediate_position.x < 0:
+				break
+			if intermediate_position.y >= shape.y or intermediate_position.y < 0:
+				break
+			if check_colision(intermediate_position):
+				for child in get_children():
+					if child.grid_position == intermediate_position:
+						if child.player != selected.player:
+							print('attack',intermediate_position)
+							end_positions += [intermediate_position]
+						print('collision', intermediate_position)
+						colision = true
+						break
+			if colision == true:
+				break
+	return end_positions
 
 func proccess_moves(initial_position, moves, repeat):
 	var end_positions = []
@@ -77,12 +128,17 @@ func proccess_moves(initial_position, moves, repeat):
 				break
 			if intermediate_position.y >= shape.y or intermediate_position.y < 0:
 				break
+			if check_colision(intermediate_position):
+				break
 			end_positions += [intermediate_position]
+				
 			
 
 	return end_positions
 
 func _input(event):
+	if game_end:
+		return
 	if event is InputEventMouseButton and event.pressed:
 		var mousepos
 		if not walking:
@@ -93,19 +149,60 @@ func _input(event):
 					if world_to_map(mousepos) in valid_moves:
 						walking = true
 						target = (cell_size* Vector2(1,1)/2) + map_to_world(world_to_map(mousepos))
-						print(target)
+						for move in valid_moves:
+							var cellv = get_cellv(move)
+							set_cellv(move, cellv-2)
+						for move in valid_attacks:
+							var cellv = get_cellv(move)
+							set_cellv(move, cellv-4)
+						current_player = 'black' if current_player == 'white' else 'white'
+						return
+					if world_to_map(mousepos) in valid_attacks:
+						walking = true
+						target = (cell_size* Vector2(1,1)/2) + map_to_world(world_to_map(mousepos))
+						for child in get_children():
+							if child.grid_position == world_to_map(mousepos):
+								if child.type == 'king':
+									game_end =true
+									print('GAME OVER')
+								remove_child(child)
+						for move in valid_moves:
+							var cellv = get_cellv(move)
+							set_cellv(move, cellv-2)
+						for move in valid_attacks:
+							var cellv = get_cellv(move)
+							set_cellv(move, cellv-4)
+						current_player = 'black' if current_player == 'white' else 'white'
+						
 			else:
 				if select(mousepos):
+					if selected.player != current_player:
+						selected = null
+						return
 					# var list_of_valid_moves = []
-					valid_moves = selected.get_moves()
+					valid_moves = selected.get_moves() + selected.get_special_moves()
+					valid_attacks = selected.get_attacks()
 					move_repeat = selected.move_repeat()
 					valid_moves = proccess_moves(selected.grid_position, valid_moves, move_repeat)
+					valid_attacks = process_attacks(selected.grid_position, valid_attacks, move_repeat)
 #					print(list_of_valid_moves)
+					for move in valid_moves:
+						var cellv = get_cellv(move)
+						set_cellv(move, cellv+2)
+					for move in valid_attacks:
+						var cellv = get_cellv(move)
+						set_cellv(move, cellv+4)
 					print(valid_moves)
 					print(move_repeat)
+					print(valid_attacks)
 					
 				
 
+func check_colision(gridpos):
+	for child in get_children():
+		if child.grid_position == gridpos:
+			return true
+	return false
 
 func select(mousepos):
 	if selected == null:
@@ -116,7 +213,7 @@ func select(mousepos):
 	return false
 
 func disable_selection(mousepos):
-	if selected.position == world_to_map(mousepos):
+	if selected.grid_position == world_to_map(mousepos):
 		selected = null
 		return false
 	return true
